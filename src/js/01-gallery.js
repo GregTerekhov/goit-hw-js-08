287e375bf8c44ad62bae5e20429ada291e4ab82a
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-02-18 18:19:55 +0200

    Add second and third exercises

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCards(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryCards(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+        `;
+    })
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery__item', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
 // Change code below this line
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInputData), 500);
+
+outputData();
+
+function onInputData(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(parsedData);
+}
+
+function outputData() {
+  if (Object.keys(parsedData)) {
+    formData.map(({ element }) => {
+      document.querySelector(`[name = '${element}']`).value =
+        parsedData[element];
+    });
+  }
+}
