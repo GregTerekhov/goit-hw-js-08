90164c180832513b4972116df563919fa6c03b9e
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-02-20 20:59:19 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,6 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
 
 outputData();
 
@@ -11,23 +10,24 @@ form.addEventListener('input', throttle(onInputData), 500);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log(formData);
-  // event.currentTarget.reset();
+  event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function onInputData(event) {
-  formData[event.target.name] = event.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  let inputData = localStorage.getItem(STORAGE_KEY);
+  inputData = inputData ? JSON.parse(inputData) : {};
+  inputData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
 }
 
 function outputData() {
   let outputData = localStorage.getItem(STORAGE_KEY);
   if (outputData) {
     outputData = JSON.parse(outputData);
-    Object.entries(formData).forEach(([name, value]) => {
-      formData[name] = value;
+    Object.entries(outputData).forEach(([name, value]) => {
       form.elements[name].value = value;
+      console.log(outputData);
     });
   }
 }
