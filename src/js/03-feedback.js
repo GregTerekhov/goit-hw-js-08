66e2d5ea103ab250d5dda7f22685fd9d2ddf4dd2
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-02-20 03:55:39 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,26 +5,29 @@ const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
 const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
+outputData();
+
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onInputData), 500);
-
-outputData();
 
 function onInputData(event) {
   formData[event.target.name] = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  console.log(formData);
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
 }
 
 function outputData() {
   if (localStorage.keys) {
-    const outputData = Object.keys(parsedData);
-    console.log(outputData);
+    const formDataKeys = Object.entries(parsedData);
+    formDataKeys.map(element => {
+      document.querySelector(`[name = '${element}']`).value =
+        parsedData[element];
+    });
   }
 }
